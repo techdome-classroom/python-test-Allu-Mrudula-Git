73e9d633e330d697c2fdfcd65f890cfe7fbd87db
Allu-Mrudula-Git <allumrudula60@gmail.com>
2024-05-08 00:43:03 -0500

    Auto commit on save

--- program1.py
+++ program1.py
@@ -16,7 +16,7 @@ class Solution:
                         q.pop(0)
                         if(p[0]>0 and grid[p[0]-1][q[0]]=='L'):
                             q.append((p[0]-1, q[0]))
-                        
+                        if(p[0]<n and )
 
 
         return 0
