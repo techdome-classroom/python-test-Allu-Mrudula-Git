0f8106707adde67578473f1ba962b368ecf20e6a
Allu-Mrudula-Git <allumrudula60@gmail.com>
2024-05-08 00:48:26 -0500

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,6 +1,35 @@
-class Solution:
+# class Solution:
    
-    def getTotalIsles(self, grid: list[list[str]]) -> int:
+#     def getTotalIsles(self, grid: list[list[str]]) -> int:
+#     #    write your code here
+#         dirs= [(0,1),(0,-1),(1,0),(-1,0)]
+#         n= len(grid)
+#         m= len(grid[0])
+#         ans= 0
+#         for i in range(n):
+#             for j in range(m):
+#                 if grid[i][j]== 'L':
+#                     ans+=1
+#                     q= [(i,j)]
+#                     while(len(q)>0):
+#                         p= q[0]
+#                         grid[p]= 'W'
+#                         q.pop(0)
+#                         if(p[0]>0 and grid[p[0]-1][p[1]]=='L'):
+#                             q.append((p[0]-1, p[1]))
+#                         if(p[0]<n and grid[p[0]+1][p[1]]=='L'):
+#                             q.append((p[0]+1, p[1]))
+#                         if(p[1]>0 and grid[p[0]][p[1]-1]=='L'):
+#                             q.append((p[0], p[1]-1))
+#                         if(p[1]<m and grid[p[0]][p[1]+1]=='L'):
+#                             q.append((p[0], p[1]+1))
+        
+#         print(ans)
+
+#         return ans
+
+
+def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here
         dirs= [(0,1),(0,-1),(1,0),(-1,0)]
         n= len(grid)
@@ -27,7 +56,4 @@ class Solution:
         print(ans)
 
         return ans
-
-Solution s
-print(s.ge)
     
