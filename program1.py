f2f3b5418b292182e214b0296628f9422b9004bc
Allu-Mrudula-Git <allumrudula60@gmail.com>
2024-05-08 00:44:43 -0500

    Auto commit on save

--- program1.py
+++ program1.py
@@ -20,7 +20,7 @@ class Solution:
                         if(p[0]<n and grid[p[0]+1][p[1]]=='L'):
                             q.append((p[0]+1, p[1]))
                         if(p[1]>0 and grid[p[0]][p[1]-1]=='L'):
-                            q.append()
+                            q.append((p[0], p[1]-1))
 
 
         return 0
