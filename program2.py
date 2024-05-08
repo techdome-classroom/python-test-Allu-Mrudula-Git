df6c9f8187f90ccfddecd56825ee17b4fd90fd42
Allu-Mrudula-Git <allumrudula60@gmail.com>
2024-05-08 00:25:32 -0500

    Auto commit on save

--- program2.py
+++ program2.py
@@ -11,7 +11,8 @@ def decode_message( s: str, p: str) -> bool:
                                 if(dp[i-1][j-1]==1):
                                         break
                                 j+=1
-                        for j in 
+                        while(j< ls+1):
+                                dp[i][j]=1 
                 for j in range(1, ls+1):
                         if(p[i-1]=='?' or (p[i-1]== s[j-1])):
                                 dp[i][j]= dp[i-1][j-1]
