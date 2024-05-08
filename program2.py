b54a744aab2642bab4aacafe555a9a8515281468
Allu-Mrudula-Git <allumrudula60@gmail.com>
2024-05-08 00:25:52 -0500

    Auto commit on save

--- program2.py
+++ program2.py
@@ -13,11 +13,12 @@ def decode_message( s: str, p: str) -> bool:
                                 j+=1
                         while(j< ls+1):
                                 dp[i][j]=1 
-                for j in range(1, ls+1):
-                        if(p[i-1]=='?' or (p[i-1]== s[j-1])):
-                                dp[i][j]= dp[i-1][j-1]
+                else:
+                        for j in range(1, ls+1):
+                                if(p[i-1]=='?' or (p[i-1]== s[j-1])):
+                                        dp[i][j]= dp[i-1][j-1]
 
 
-        return dp
+        return 
 
 print(decode_message('abaacd', 'ab*?c?*'))
